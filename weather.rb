e6934469514620e0e5a4a894ab6eac8ce6911f96
AnnabelleCat <pikainka@gmail.com>
2018-07-11 20:48:42 +0000

    automatically backed up by learn

--- weather.rb
+++ weather.rb
@@ -6,10 +6,20 @@
 # 	:temperature_farenheit => 66.3
 # }
 
+
+
 # 1. Complete the method below called location, that accepts a hash called weather as an argument so that it returns the value of city from the weather hash. 
 def location(weather)
+  nyc_weather = {
+    :city => "New York"
+    :description => "Partly Cloudy"
+    :temperature_farenheit => 66.3
+  }
+  puts nyc_weather[:city]
 	
 end
+
+location(nyc_weather)
 
 # 2. Using the location method as a guide, write a  method called "description" so it returns the description from the weather hash. 
 
